b5d3fdec3603e1137ff098f30c71dd41ab7b32e7
KeerthanaMadhavan <keerthanamadhavan@KeerthaavansAir.lan>
2024-04-01 00:32:33 -0400

    working on Gamepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { GamePageComponent } from './game-page/game-page.component';
+import { CommonModule, NgFor, NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, MatToolbarModule, GamePageComponent, NgFor],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
